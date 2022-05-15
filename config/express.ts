d62e473c818b5e130de1dcf4757484e35b51521d
Norbey Vasquez <lic.vasquezt@gmail.com>
2022-05-15 14:19:30 -0500

    hotfix: changes in CORS policy

--- config/express.ts
+++ config/express.ts
@@ -1,6 +1,14 @@
 import express, { Express } from 'express';
+import cors from 'cors';
 
 function configExpress(app: Express) {
+  const corsOptions ={
+    origin:'*',
+    credentials:true,
+    optionSuccessStatus:200,
+ }
+
+  app.use(cors(corsOptions));
   app.use(express.json());
 }
 
